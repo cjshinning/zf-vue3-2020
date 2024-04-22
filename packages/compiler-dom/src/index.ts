d852a4b847d8893ee8a0604e17d49a6041e0e7e5
cjshinningg <784319417@qq.com>
2024-04-22 22:20:33 +0800

    62.ast解析表达式和元素

--- packages/compiler-dom/src/index.ts
+++ packages/compiler-dom/src/index.ts
@@ -14,15 +14,81 @@ export const enum NodeTypes {
 
 function isEnd(context) { //是否解析完毕，解析完毕的核心就是context.source=''
   const source = context.source;
+
+  if (source.startsWith('</')) {
+    return true;
+  }
+
   return !source;
 }
 
-function parseElement(context) {
+function advanceSpaces(context) {
+  const match = /^[ \t\r\n]+/.exec(context.source);
+  if (match) {
+    advanceBy(context, match[0].length);
+  }
+}
 
+function parseTag(context) {
+  const start = getCursor(context);
+  const match = /^<\/?([a-z][^ \t\r\n\>]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceSpaces(context);
+
+  const isSelfClosing = context.source.startsWith('/>');
+  advanceBy(context, isSelfClosing ? 2 : 1)
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+    isSelfClosing,
+    loc: getSelection(context, start)
+  }
+}
+
+function parseElement(context) {  //{{   name  }} {{name}}
+  // 1.解析标签的名字
+  let ele: any = parseTag(context);  //这里处理儿子有可能没有儿子，如果遇到结束标签就直接跳出
+
+  // 2.处理儿子
+  const children = parseChildren(context);
+
+  if (context.source.startsWith('</')) {
+    parseTag(context);  //解析关闭标签时，同时会移除关闭信息，并且更新偏移量
+  }
+  ele.children = children;
+  ele.loc = getSelection(context, ele.loc.start);
+  return ele;
 }
 
 function parseInterpolation(context) {
+  const start = getCursor(context); //获取表达式的start位置
+  const closeIndex = context.source.indexOf('}}', '{{');
 
+  advanceBy(context, 2);
+  const innerStart = getCursor(context);
+  const innerEnd = getCursor(context);
+  const rawContentLength = closeIndex - 2;  //拿到{{ 内容 }}，包含空格的
+  const preTrimContent = parseTextData(context, rawContentLength);
+  const content = preTrimContent.trim();
+  const startOffset = preTrimContent.indexOf(content);
+  if (startOffset > 0) {  //前面有空格
+    advancePositionWithMutation(innerStart, preTrimContent, startOffset);
+  }
+  // 再去更新innerEnd
+  const endOffset = content.length + startOffset;
+  advancePositionWithMutation(innerEnd, preTrimContent, endOffset);
+  advanceBy(context, 2);
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      isStatic: false,
+      loc: getSelection(context, innerStart, innerEnd)
+    },
+    loc: getSelection(context, start)
+  }
 }
 
 function getCursor(context) {
@@ -43,7 +109,6 @@ function advancePositionWithMutation(context, s, endIndex) {
   context.offset += endIndex;
   context.line += lineCount;
   context.column = linePos == -1 ? context.column + endIndex : endIndex - linePos;
-  console.log(context);
   // 如何更新列数
   // 如何更新偏移量
 
@@ -64,8 +129,8 @@ function parseTextData(context, endIndex) {
   return rawText;
 }
 
-function getSelection(context, start) { //获取这个信息对应的开始、结束、内容
-  let end = getCursor(context);
+function getSelection(context, start, end?) { //获取这个信息对应的开始、结束、内容
+  end = end || getCursor(context);
   return {
     start,
     end,
@@ -107,10 +172,20 @@ function parseChildren(context) {
     } else {
       node = parseText(context);
     }
+    // break;
     nodes.push(node);
   }
+  nodes.forEach((node, index) => {
+    if (node.type === NodeTypes.TXET) {
+      if (!/[^ \t\r\n]/.test(node.content)) {
+        nodes[index] = null;
+      } else {
+        node.content = node.content.replace(/[ \t\r\n]+/g, ' ');
+      }
+    }
+  })
 
-  return nodes;
+  return nodes.filter(Boolean);
 }
 
 function createParserContext(content) {
@@ -123,11 +198,20 @@ function createParserContext(content) {
   }
 }
 
+function createRoot(children, loc) {
+  return {
+    type: NodeTypes.ROOT,
+    children,
+    loc
+  }
+}
+
 function baseParse(content) {
   // 标识节点的信息 行、列、偏移量...
   // 每解析一段，就移除一部分
   const context = createParserContext(content);
-  return parseChildren(context);
+  const start = getCursor(context); //记录开始位置
+  return createRoot(parseChildren(context), getSelection(context, start));
 }
 
 export function baseCompile(template) {
@@ -135,3 +219,6 @@ export function baseCompile(template) {
   const ast = baseParse(template);
   return ast;
 }
+
+// 从template -> ast语法树（vue里面有指令、有插槽、有事件，ast语法树是无法全部展示的）
+// ast语法树 -> transform -> codegen
